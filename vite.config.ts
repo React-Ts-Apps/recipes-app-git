b42a3ca40a3c0ca068ba25050e2c3ceda9cd6f29
shijibijo84 <shijib.bijo@gmail.com>
2025-05-08 16:12:04 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/recipes-app-git/",
+  build: {
+    outDir: "dist",  // Ensure the build is output into the 'dist' directory
+    assetsDir: "assets",  // Optionally, specify the assets directory
+  },
 });
