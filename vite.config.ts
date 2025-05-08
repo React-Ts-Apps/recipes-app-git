f14902f56821a780220b9ffcc27aa3ca246f0267
shijibijo84 <shijib.bijo@gmail.com>
2025-05-08 16:50:23 +0200

    remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/recipes-app-git/",
-  build: {
-    outDir: "dist",  // Ensure the build is output into the 'dist' directory
-    assetsDir: "assets",  // Optionally, specify the assets directory
-  },
+  base: "/",
+
 });
